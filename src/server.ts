8153526040c68c6040b46382e6056f2df88d7460
Lucas Torres <lucascms11@gmail.com>
2022-11-13 14:38:52 -0300

    [CHANGE] Adicionando bibliotecas de desenvolvimento e rotas da API.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import { AppDataSource } from "./data-source"
+import * as morgan from "morgan";
 import * as express from "express";
+import * as cors from "cors"
+import routes from "./routes";
 
 AppDataSource.initialize().then(async () => {
     const app = express();
 
     app.use(express.json)
+    app.use(morgan('dev'))
+    app.use(cors())
+    app.use(routes)
 
-    return app.listen(process.env.PORT)
+    return app.listen(process.env.PORT, () => console.log(`Aplicação online na porta ${process.env.PORT}`))
 }).catch(error => console.log(error))
